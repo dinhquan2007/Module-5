7d9f1b382f646e51a611299b9ae1658f29f863f0
dinhquan2007 <dinhquan2007@gmail.com>
2023-09-20 20:05:12 +0700

    fix loi component

--- ss3_componont/todo-app/src/App.js
+++ ss3_componont/todo-app/src/App.js
@@ -1,44 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-import {Component} from "react";
+import Todo from "./component/Todo";
 
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            list: ["lau nhà"],
-            item: ""
-        }
-    }
-
-    handleChange = (event) => {
-        this.setState({item: event.target.value})
-    }
-    handleAddItem = () => {
-        if (this.state.item !== "") {
-            this.setState({
-                list: [...this.state.list, this.state.item],
-                item: ""
-            })
-        }
-    }
-
-    render() {
+function App(){
         return (
             <div className="App">
-                <h1>Todo list</h1>
-                <input type="text" id="event" value={this.state.item} onChange={this.handleChange}/>
-                <button onClick={this.handleAddItem}>Add</button>
-                <ul>
-                    {this.state.list.map((e, index) => (
-                            <li key={index}>{e}</li>
-                        )
-                    )}
-                </ul>
+                <Todo/>
             </div>
         );
-    }
 }
 
 export default App;
---
+++ ss3_componont/todo-app/src/component/Todo.js
@@ -0,0 +1,41 @@
+import {Component} from "react";
+
+class Todo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            list: ["lau nhà"],
+            item: ""
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({item: event.target.value})
+    }
+    handleAddItem = () => {
+        if (this.state.item !== "") {
+            this.setState({
+                list: [...this.state.list, this.state.item],
+                item: ""
+            })
+        }
+    }
+
+    render() {
+        return (
+            <div className="Todo">
+                <h1>Todo list</h1>
+                <input type="text" id="event" value={this.state.item} onChange={this.handleChange}/>
+                <button onClick={this.handleAddItem}>Add</button>
+                <ul>
+                    {this.state.list.map((e, index) => (
+                            <li key={index}>{e}</li>
+                        )
+                    )}
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Todo;
